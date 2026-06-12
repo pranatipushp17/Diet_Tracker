d93f0f556b01035c2f141d29c757d43a809b9ef3
pranatipushp17 <pranatipushp17@gmail.com>
2026-06-12 12:50:20 +0530

    updated api url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://diet-tracker-d22u.onrender.com/api'
 })
 
 // har request mein automatically token lagao
